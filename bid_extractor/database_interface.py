057dbeba3aa7b02c4be76987881bb9065a4a0905
Justin <kellerjustin@protonmail.com>
2021-11-21 17:45:09 -0600

    fixes wrong commodities

--- bid_extractor/database_interface.py
+++ bid_extractor/database_interface.py
@@ -27,8 +27,8 @@ def enter_grain_bids(conn, commodity, date, price):
     :param price:
     :return:
     """
-    COMMODITY_GUID = {"corn": '05b83824752d49039bd436244c4f4729',
-                      "soybeans": '9674359206294b7293e00c8bc317b9b0'
+    COMMODITY_GUID = {"corn": '9674359206294b7293e00c8bc317b9b0',
+                      "soybeans": '05b83824752d49039bd436244c4f4729',
                      }
     CURRENCY_GUID = 'a5cb4782e9d344e693e7c99cb35eb687'
     SOURCE = 'user:price-editor'
@@ -52,33 +52,7 @@ def enter_grain_bids(conn, commodity, date, price):
                      {price},
                      {VALUE_DENOM})
             """
-    return sql
-    # cur = conn.cursor()
-    # cur.execute(sql)
-    # conn.commit()
-    # return cur.lastrowid
-
-
-"""
-def main():
-    database = r"C:\sqlite\db\pythonsqlite.db"
-
-    # create a database connection
-    conn = create_connection(database)
-    with conn:
-        # create a new project
-        project = ('Cool App with SQLite & Python', '2015-01-01', '2015-01-30');
-        project_id = create_project(conn, project)
-
-        # tasks
-        task_1 = ('Analyze the requirements of the app', 1, 1, project_id, '2015-01-01', '2015-01-02')
-        task_2 = ('Confirm with user about the top requirements', 1, 1, project_id, '2015-01-03', '2015-01-05')
-
-        # create tasks
-        create_task(conn, task_1)
-        create_task(conn, task_2)
-"""
-
-if __name__ == '__main__':
-    # main()
-    print(enter_grain_bids("hi", "soybeans", "2021-02-10", 558))
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+    return cur.lastrowid
