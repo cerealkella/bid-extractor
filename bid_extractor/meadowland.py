85425987d57017027b9b1b27626f1820ee275b45
Justin <kellerjustin@protonmail.com>
2021-11-23 22:44:08 -0600

    meadowland updated their site

--- bid_extractor/meadowland.py
+++ bid_extractor/meadowland.py
@@ -1,28 +1,38 @@
+import codecs
+import os
+import datetime
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import wget
+from .local_settings import DOWNLOAD_FOLDER
 
 
-def extract_price(url, contract_code):
-    price_idx = -1
-    price = 0
+NOW = datetime.datetime.now()
+TODAY = NOW.strftime("%Y-%m-%d")
+
+def get_cashbid_csv():
+    print(f"{NOW} - Saving Meadowland Cash Bids CSV file...")
+    url = "https://www.meadowlandfarmerscoop.com/markets/cashbid-download.php"
+    wget.download(url, out=os.path.join(DOWNLOAD_FOLDER,f"{TODAY}-Meadowland.csv"), bar=None)
+
+
+def get_cashbid_html():
     driver = webdriver.Firefox()
+    driver.implicitly_wait(0.5)
+    #launch URL
+    url = "https://www.meadowlandfarmerscoop.com/markets/cash.php"
     driver.get(url)
-    print(driver.title)
-    for row in driver.find_elements_by_css_selector("tr"):
-        for index, cell in enumerate(row.find_elements_by_tag_name("td")):
-            # print(f"{index} - {cell.text}")
-            if cell.text == contract_code:
-                price_idx = index + 3
-            if index == price_idx:
-                price = cell.text  # float(str(cell.text.strip()))
-                driver.close()
-                return price
+    #get file path to save page
+    n=os.path.join(DOWNLOAD_FOLDER,f"{TODAY}-Meadowland.html")
+    #open file in write mode with encoding
+    print(f"{NOW} - Saving Meadowland Cash Bids HTML file...")
+    f = codecs.open(n, "w", "utf−8")
+    #obtain page source
+    h = driver.page_source
+    #write page source content to file
+    f.write(h)
+    #close browser
+    driver.quit()
 
 
-bean_url = "https://www.meadowlandfarmerscoop.com/index.cfm?show=11&mid=3&theLocation=9&layout=1046"
-bean_price = extract_price(bean_url, "@S2F")
-print(f"Meadowland Bean Price is {bean_price}")
-
-corn_url = "https://www.meadowlandfarmerscoop.com/index.cfm?show=11&mid=3&theLocation=7&layout=1046"
-corn_price = extract_price(corn_url, "@C1Z")
-print(f"Meadowland Corn Price is {corn_price}")
+get_cashbid_csv()
+get_cashbid_html()
